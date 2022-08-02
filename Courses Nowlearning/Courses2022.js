49a4700e84c9b28aa5c80c4103bbda4dc2c4c790
Jose Guerra <58611147+jdpguerra@users.noreply.github.com>
2022-08-02 12:03:57 -0300

    courses now
    update v1

--- Courses Nowlearning/Courses2022.js
+++ Courses Nowlearning/Courses2022.js
@@ -1,3 +1,14 @@
 //ServiceNow Fundamentals On Demand
+var fundamentals = "https://nowlearning.servicenow.com/lxp?id=learning_path_prev&path_id=6b78901c1b748050b1c7fe631a4bcb73";
 
-//https://nowlearning.servicenow.com/lxp?id=learning_path_prev&path_id=6b78901c1b748050b1c7fe631a4bcb73
+//Vulnerability Response (VR) Implementation On Demand
+var vr = "https://nowlearning.servicenow.com/lxp?course_id=55f6f4d21b0ac1109098c8ca234bcbe8&id=learning_course_prev";
+
+//Vendor Risk Management (VRM) Implementation On Demand
+var vrm = "https://nowlearning.servicenow.com/lxp?course_id=acf35391db46c590421266f7489619ba&id=learning_course_prev";
+
+//System Administration Advanced On Demand
+var sad = "https://nowlearning.servicenow.com/lxp?id=learning_course_prev&course_id=d3b525f11b9c8d5475950dc0604bcbee";
+
+//Service Portal Fundamentals On Demand
+var spf = "https://nowlearning.servicenow.com/lxp?course_id=3d87783fdb558d50454eb0a239961970&id=learning_course_prev";
