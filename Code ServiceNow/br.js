323e5dbff4c4f774a1ee81084a995bfa610cb5d9
Jose Guerra <58611147+jdpguerra@users.noreply.github.com>
2022-08-03 23:40:45 -0300

    v4
    v4

---
+++ Code ServiceNow/br.js
@@ -0,0 +1,5 @@
+//Preventing recursion in Business Rules
+
+current.setValue("state", 1);
+current.setWorkflow(false);
+current.update();
---
+++ Code ServiceNow/developer.js
@@ -0,0 +1,1 @@
+//https://developer.servicenow.com/dev.do#!/reference/api/sandiego/client/c_GlideFormAPI
