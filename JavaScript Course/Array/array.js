02ff37a9c073592b75034658ab30ae5c3ec6ec44
Jose Guerra <58611147+jdpguerra@users.noreply.github.com>
2022-08-05 22:38:44 -0300

    version array v4
    v4 version array

---
+++ JavaScript Course/Array/array.js
@@ -0,0 +1,2 @@
+let arr = ["Jack", "Mike", "Steve"];
+document.getElementById("arrPrint").innerHTML = arr;
--- JavaScript Course/links.js
+++ JavaScript Course/links.js
@@ -1,3 +1,4 @@
 //https://www.javascript.com/
 //https://blog.betrybe.com/javascript/
 //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide
