12c9f5780b6eb0d2c42545e9e577b135afe755fb
Jose Guerra <58611147+jdpguerra@users.noreply.github.com>
2022-08-08 23:52:48 -0300

    update v1
    v1

--- JavaScript Course/Array/array.js
+++ JavaScript Course/Array/array.js
@@ -1,2 +1,10 @@
 let arr = ["Jack", "Mike", "Steve"];
 document.getElementById("arrPrint").innerHTML = arr;
+
+let arr1 = ["Jack", "Mike", "Steve"];
+document.getElementById("arrPrint").innerHTML = arr;
+
+let arr2 = ["Jack", "Mike", "Steve"];
+document.getElementById("arrPrint").innerHTML = arr;
+let arr3 = ["Jack", "Mike", "Steve"];
+document.getElementById("arrPrint").innerHTML = arr;
