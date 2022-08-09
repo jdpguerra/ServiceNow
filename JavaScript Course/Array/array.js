384fe70bc9e3c61d20be1fa28bf0d9afcd768ca2
Jose Guerra <58611147+jdpguerra@users.noreply.github.com>
2022-08-08 23:53:52 -0300

    array v1
    v1 update

--- JavaScript Course/Array/array.js
+++ JavaScript Course/Array/array.js
@@ -8,3 +8,23 @@ let arr2 = ["Jack", "Mike", "Steve"];
 document.getElementById("arrPrint").innerHTML = arr;
 let arr3 = ["Jack", "Mike", "Steve"];
 document.getElementById("arrPrint").innerHTML = arr;
+
+let arr4 = ["Jack", "Mike", "Steve"];
+document.getElementById("arrPrint").innerHTML = arr;
+let arr5 = ["Jack", "Mike", "Steve"];
+document.getElementById("arrPrint").innerHTML = arr;
+
+let arr6 = ["Jack", "Mike", "Steve"];
+document.getElementById("arrPrint").innerHTML = arr;
+let arr7 = ["Jack", "Mike", "Steve"];
+document.getElementById("arrPrint").innerHTML = arr;
+
+let arr8 = ["Jack", "Mike", "Steve"];
+document.getElementById("arrPrint").innerHTML = arr;
+let arr9 = ["Jack", "Mike", "Steve"];
+document.getElementById("arrPrint").innerHTML = arr;
+
+let arr10 = ["Jack", "Mike", "Steve"];
+document.getElementById("arrPrint").innerHTML = arr;
+let arr11 = ["Jack", "Mike", "Steve"];
+document.getElementById("arrPrint").innerHTML = arr;
