69cbbd9c6d42d5aa9875220ed84a000f049a4add
Jose Guerra <58611147+jdpguerra@users.noreply.github.com>
2022-10-17 15:05:08 -0300

    shortcuts
    v1

---
+++ Code ServiceNow/Shortcuts/Shortcuts.js
@@ -0,0 +1,25 @@
+//Code in filter navidator
+
+//Example name this instance version
+//stats.do
+
+//table incident 
+//incident.list
+
+//table new application
+//incident.LIST
+
+//form table application
+//incident.form
+
+//incident form new browser
+//incident.FORM
+
+//new record incident list
+//incident.do
+
+//new record  new browser
+//incident.DO
+
+//navigation table empty info
+//incident.filter
